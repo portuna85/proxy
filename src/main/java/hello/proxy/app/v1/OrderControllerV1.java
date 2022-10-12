2927500e640b8a1ff5885b74f72159c4b279e77c
portuna85 <portuna85@gmail.com>
2022-10-12 16:27:10 +0900

    Dynamic Proxy pattern

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
-    String request(@RequestParam String itemId);
+    String request(@RequestParam("itemId") String itemId);
 
     @GetMapping("/v1/no-log")
     String noLog();
